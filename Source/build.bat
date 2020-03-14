11c09722be546022568f337a4d1951d54cc63072
i-saint <saint.skr@gmail.com>
2020-03-14 13:46:42 +0900

    build script

--- Source/build.bat
+++ Source/build.bat
@@ -7,32 +7,29 @@ exit /B 0
 :DoBuild
     set MQ_VERSION=%~1
     set MQ_SDK_DIR=%cd%\Externals\mqsdk%MQ_VERSION%\mqsdk
-    msbuild mqabcPlayer.vcxproj /t:Build /p:Configuration=Release /p:Platform=x64 /m /nologo
+    msbuild mqusdCore.vcxproj /t:Build /p:Configuration=Release /p:Platform=x64 /m /nologo
     IF %ERRORLEVEL% NEQ 0 (
         pause
         exit /B 1
     )
-    msbuild mqabcRecorder.vcxproj /t:Build /p:Configuration=Release /p:Platform=x64 /m /nologo
+    msbuild mqusdPlayer.vcxproj /t:Build /p:Configuration=Release /p:Platform=x64 /m /nologo
     IF %ERRORLEVEL% NEQ 0 (
         pause
         exit /B 1
     )
-    msbuild mqabcPlayer.vcxproj /t:Build /p:Configuration=Release /p:Platform=Win32 /m /nologo
+    msbuild mqusdRecorder.vcxproj /t:Build /p:Configuration=Release /p:Platform=x64 /m /nologo
     IF %ERRORLEVEL% NEQ 0 (
         pause
         exit /B 1
     )
-    msbuild mqabcRecorder.vcxproj /t:Build /p:Configuration=Release /p:Platform=Win32 /m /nologo
-    IF %ERRORLEVEL% NEQ 0 (
-        pause
-        exit /B 1
-    )
-    set DIST_DIR64="_dist\mqabc_Windows_64bit"
-    set DIST_DIR32="_dist\mqabc_Windows_32bit"
+    set DIST_DIR64="_dist\mqusd_Windows_64bit"
+    set CORE_DIR64="%DIST_DIR64%\mqusdCore"
     mkdir "%DIST_DIR64%"
-    mkdir "%DIST_DIR32%"
-    copy _out\x64_Release\mqabcPlayer%MQ_VERSION%\mqabcPlayer.dll "%DIST_DIR64%"
-    copy _out\x64_Release\mqabcRecorder%MQ_VERSION%\mqabcRecorder.dll "%DIST_DIR64%"
-    copy _out\Win32_Release\mqabcPlayer%MQ_VERSION%\mqabcPlayer.dll "%DIST_DIR32%"
-    copy _out\Win32_Release\mqabcRecorder%MQ_VERSION%\mqabcRecorder.dll "%DIST_DIR32%"
+    mkdir "%CORE_DIR64%"
+    copy _out\x64_Release\%MQ_VERSION%\mqusdPlayer.dll "%DIST_DIR64%"
+    copy _out\x64_Release\%MQ_VERSION%\mqusdRecorder.dll "%DIST_DIR64%"
+    copy _out\x64_Release\%MQ_VERSION%\mqusdCore.dll "%CORE_DIR64%"
+    copy Externals\x64\lib\tbb.dll "%CORE_DIR64%"
+    copy Externals\x64\lib\usd_ms.dll "%CORE_DIR64%"
+    xcopy /EI Externals\x64\lib\usd "%CORE_DIR64%\usd"
     exit /B 0
